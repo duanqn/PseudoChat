e932c5a2ab7fb03a5f55c4c969cf4d4b35c3ad42
Qingnan Duan <duanqn_own_1@yeah.net>
2021-12-27 17:19:57 +0800

    Add navigation to chat page

--- PseudoChat/PseudoChat/Settings.xaml.cpp
+++ PseudoChat/PseudoChat/Settings.xaml.cpp
@@ -6,6 +6,7 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace winrt::Microsoft::UI::Xaml::Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,12 +25,45 @@ namespace winrt::PseudoChat::implementation
         auto&& resource = resourceMap.GetValue(L"StringTable/SearchPlaceholder", context);
         
         searchBox.PlaceholderText(resource.ValueAsString());
+
+        auto&& navChatItem = navChat();
+        resource = resourceMap.GetValue(L"StringTable/navChatText", context);
+        navChatItem.Content(winrt::box_value(resource.ValueAsString()));
     }
 
-    void Settings::OnRootFrameNavigated(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs const&)
+    void Settings::OnDisplayFrameNavigated(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs const&)
     {
     }
     void Settings::CtrlF_Invoked(Microsoft::UI::Xaml::Input::KeyboardAccelerator const&, Microsoft::UI::Xaml::Input::KeyboardAcceleratorInvokedEventArgs const&)
     {
+    }
+
+    void Settings::NavigationViewControl_ItemInvoked(NavigationView const& sender, NavigationViewItemInvokedEventArgs const& args)
+    {
+        if (args.InvokedItemContainer().IsSelected()) {
+            // Selecting an item that is already selected.
+            return;
+        }
+
+        if (args.IsSettingsInvoked()) {
+            // TODO: Navigate to settings page.
+            return;
+        }
+
+        // Figure out which item is invoked.
+        auto&& invokedItem = args.InvokedItem();
+        winrt::Windows::Foundation::PropertyType propertyType = invokedItem.as<winrt::Windows::Foundation::IPropertyValue>().Type();
+        // `invokedItem` is the boxed NavigationViewItem.Content
+        // Here we assert that it is a winrt::hstring
+        WINRT_ASSERT(propertyType == winrt::Windows::Foundation::PropertyType::String);
+
+        // Assertion OK; Unbox it as a winrt::hstring
+        auto invokedString = winrt::unbox_value<winrt::hstring>(invokedItem);
+        
+        winrt::Microsoft::UI::Xaml::Controls::NavigationViewItem invokedViewItem = args.InvokedItemContainer().as<winrt::Microsoft::UI::Xaml::Controls::NavigationViewItem>();
+        auto&& frame = DisplayFrame();
+        if (invokedViewItem == navChat()) {
+            frame.Navigate(winrt::xaml_typename<Chat>());
+        }
     }
 }
--- PseudoChat/PseudoChat/Settings.xaml.h
+++ PseudoChat/PseudoChat/Settings.xaml.h
@@ -8,8 +8,9 @@ namespace winrt::PseudoChat::implementation
     {
         Settings();
 
-        void OnRootFrameNavigated(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs const& e);
+        void OnDisplayFrameNavigated(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs const& e);
         void CtrlF_Invoked(Microsoft::UI::Xaml::Input::KeyboardAccelerator const& sender, Microsoft::UI::Xaml::Input::KeyboardAcceleratorInvokedEventArgs const& args);
+        void NavigationViewControl_ItemInvoked(winrt::Microsoft::UI::Xaml::Controls::NavigationView const& sender, winrt::Microsoft::UI::Xaml::Controls::NavigationViewItemInvokedEventArgs const& args);
 
         private:
         winrt::Microsoft::Windows::ApplicationModel::Resources::ResourceManager m_resourceManager;
